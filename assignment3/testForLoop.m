2aa3175fd5c45f3ef573e220edc3ad2d4578adb2
Kelsey <reevesk13@students.ecu.edu>
2018-10-14 21:10:55 -0400

    Tic Tac Toe

---
+++ assignment3/testForLoop.m
@@ -0,0 +1,4 @@
+vec = [4,5,6]';
+for i = length(vec)
+    disp(vec)
+end
---
+++ letterGrade.m
@@ -0,0 +1,16 @@
+function [mean,letterGrade] = letterGrade(a,b,c)
+%function that takes 3 grades and returns the average 
+%and appropriate letter grade.
+
+    average = (a+b+c)/3;
+    
+    if (average >= 90)
+        letter = 'A';
+    elseif (average >= 80 && average < 90);
+        letter = 'B';
+    else 
+        letter = 'F'
+    end
+   
+end
+
